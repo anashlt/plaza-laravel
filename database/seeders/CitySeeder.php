a7f8ac0e41c570016a029c6417300e5455bf8e4a
anashlt <ahouloutj1@gmail.com>
2022-07-05 10:55:15 +0200

    Seeder syntax fix

--- database/seeders/CitySeeder.php
+++ database/seeders/CitySeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use DB;
+use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 
 class CitySeeder extends Seeder
@@ -20,7 +20,7 @@ class CitySeeder extends Seeder
         DB::table('cities')->insert([
             ['name' => 'London', 'created_at' => Carbon::now(), 'slug' => 'london'],
             ['name' => 'Manchester', 'created_at' => Carbon::now(), 'slug' => 'manchester'],
-            ['name' => 'Leeds', 'created_at' => Carbon::now()], 'slug' => 'leeds',
+            ['name' => 'Leeds', 'created_at' => Carbon::now(), 'slug' => 'leeds'],
             ['name' => 'Edinburgh', 'created_at' => Carbon::now(), 'slug' => 'edinburgh'],
             ['name' => 'Glasgow', 'created_at' => Carbon::now(), 'slug' => 'glasgow'],
             ['name' => 'Belfast', 'created_at' => Carbon::now(), 'slug' => 'belfast'],
